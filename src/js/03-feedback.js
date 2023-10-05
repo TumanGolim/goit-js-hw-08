c590159eb3a3eafe6cf5c330fbe7b0db703b0a4b
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-10-05 22:34:10 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageTextarea = form.querySelector('textarea[name="message"]');
+
 function saveFormData() {
   const formData = {
     email: emailInput.value,
@@ -9,3 +10,35 @@ function saveFormData() {
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
+
+function fillFormFields() {
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+}
+
+const throttledSaveFormData = throttle(saveFormData, 100);
+emailInput.addEventListener('input', throttledSaveFormData);
+messageTextarea.addEventListener('input', throttledSaveFormData);
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageTextarea.value = '';
+
+    console.log('Form Data Submitted:', formData);
+  }
+});
+
+fillFormFields();
